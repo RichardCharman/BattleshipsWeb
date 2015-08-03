be94573a0b72f9c2484dcf152fda88dc29e46b41
Richard Charman <richardcharman@live.com>
2015-08-03 22:31:49 +0000

    Added new game option, and name entry form

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,10 +1,17 @@
 require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
+  configure :development do
+  set :bind, '0.0.0.0'
+  set :port, 3000
+end
+  set :views, proc { File.join(root, '..', 'views') }
+        
   get '/' do
-    'Hello BattleshipsWeb!'
+    erb :index
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+
 end
